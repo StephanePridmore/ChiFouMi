7393d070fba7fb390895e46fedf65efd8a341bdc
stephane-pridmore <>
2017-08-16 01:37:19 +1000

    Random generator
    I did some reasurch but haven't found a good answer yet for testing this.

---
+++ ChiFouMiLibrary/HandShakeGenerator.cs
@@ -0,0 +1,21 @@
+﻿using ChiFouMiLibrary.Interfaces;
+using System;
+
+namespace ChiFouMiLibrary
+{
+    public class HandShakeGenerator : IHandShakeGenerator
+    {
+        public Shake GenerateHandShake()
+        {
+            var random = new Random();
+            var choice = random.Next(0, 2);
+
+            if (choice == 0)
+                return Shake.Paper;
+            else if (choice == 1)
+                return Shake.Rock;
+            else
+                return Shake.Scissors;
+        }
+    }
+}
