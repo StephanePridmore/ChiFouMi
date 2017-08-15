031fab1953e9fb72694e5aff372ec81609a589a9
stephane-pridmore <>
2017-08-16 00:57:21 +1000

    How should win
    For each HandShake there is a weak handShake
    
    + removing none necessary using

---
+++ ChiFouMiTest/ChiFouMiContext.cs
@@ -0,0 +1,13 @@
+﻿using ChiFouMiLibrary;
+
+namespace ChiFouMiTest
+{
+    public class ChiFouMiContext
+    {
+        public HandShake PaperShake => new HandShake(Shake.Paper, Shake.Rock);
+
+        public HandShake RockShake => new HandShake(Shake.Rock, Shake.Scissors);
+
+        public HandShake ScissorsShake => new HandShake(Shake.Scissors, Shake.Paper);
+    }
+}
--- ChiFouMiTest/ChiFouMiTests.cs
+++ ChiFouMiTest/ChiFouMiTests.cs
@@ -7,6 +7,14 @@ namespace ChiFouMiTest
 {
     class ChiFouMiTests
     {
+        public ChiFouMiContext Context;
+
+        [SetUp]
+        public void Setup()
+        {
+            Context = new ChiFouMiContext();
+        }
+
         [Test]
         public void ShouldShakeValueEqualRockPaperScissors()
         {
@@ -17,6 +25,24 @@ namespace ChiFouMiTest
         public void ShouldGameShakesHaveThreeHandShakes()
         {
             Check.That(new HandShakes().GameShakes.Count()).Equals(3);
+        }
+
+        [Test]
+        public void ShouldPaperShakeWinAgaintsRock()
+        {
+            Check.That(Context.PaperShake.WinAgaints).Equals(Shake.Rock);
+        }
+
+        [Test]
+        public void ShouldRockShakeWinAgaintsScissors()
+        {
+            Check.That(Context.RockShake.WinAgaints).Equals(Shake.Scissors);
+        }
+
+        [Test]
+        public void ShouldScissorsShakeWinAgaintsPaper()
+        {
+            Check.That(Context.ScissorsShake.WinAgaints).Equals(Shake.Paper);
         }
     }
 }
